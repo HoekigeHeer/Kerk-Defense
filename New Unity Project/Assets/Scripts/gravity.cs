66be8a4cb231c0731ac6fcef54c06171e1ad52d9
deMachtige <borisramakers99@outlook.com>
2019-06-18 16:19:29 +0200

    het project dusverre

---
+++ New Unity Project/Assets/Scripts/gravity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity : MonoBehaviour
+{
+
+    public Vector3 setGravity;
+
+    void Start()
+    {
+        Physics.gravity = setGravity;
+    }
+
+   
+}
