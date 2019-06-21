50ede5eb397637c16c3e01c08cef71c07811e94c
deMachtige <borisramakers99@outlook.com>
2019-06-21 10:05:51 +0200

    FKN HAMER TOREN BITCHBOI

---
+++ New Unity Project/Assets/Scripts/HammerTower.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerTower : MonoBehaviour
+{
+
+    public GameObject hammer;
+    bool swingBack = false;
+    bool swinging = false;
+    public float swingSpeed;
+    public float maxSwingAmount;
+    float SwingAmount = 0;
+
+    
+    void Update()
+    {
+        if (swinging)
+        {
+            if (swingBack)
+            {
+                hammer.transform.Rotate(0, 0, -swingSpeed);
+            }
+            else
+            {
+                hammer.transform.Rotate(0, 0, swingSpeed);
+            }
+
+            SwingAmount += swingSpeed;
+            if(SwingAmount >= maxSwingAmount)
+            {
+                swinging = false;
+                SwingAmount = 0;
+                swingBack = !swingBack;
+            }
+        }
+    }
+
+
+    void OnMouseDown()
+    {
+        
+        swinging = true;
+    }
+}
